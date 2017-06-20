3758d48fd72d7f3f6b120bea7fbed24ebb59bc45
Stephen Fromm <stephenf@nero.net>
2017-06-20 15:05:32 -0700

    updates to emacs loading private info
    private data loaded from the private module.  no longer tangled out with org-babel-load-file.

--- init.el
+++ init.el
@@ -3,10 +3,5 @@
 (require 'org)
 (require 'ob-tangle)
 
-; load up private configuration
-(if (file-exists-p (concat user-emacs-directory "private.org"))
-  (org-babel-load-file (concat user-emacs-directory "private.org"))
-  )
-
 ; load up the bulk of my configuration
 (org-babel-load-file (concat user-emacs-directory "emacs.org"))
