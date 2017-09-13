19ee99079dc2ad3b956a3abca2b3336b3d492162
Stephen Fromm <stephenf@nero.net>
2017-09-13 09:47:14 -0700

    continue the migration; move core config pieces into setup-core

--- init.el
+++ init.el
@@ -3,5 +3,8 @@
 (require 'org)
 (require 'ob-tangle)
 
+; update load path to pull in tangled configs
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
 ; load up the bulk of my configuration
 (org-babel-load-file (concat user-emacs-directory "emacs.org"))
