8227e8941c938c6cfa98ac1097025ca5c69b6957
Stephen Fromm <stephenf@nero.net>
2018-09-17 16:41:57 -0700

    whitespace jujitsu on dired

--- modules/forge-dired.el
+++ modules/forge-dired.el
@@ -26,45 +26,45 @@
   (setq insert-directory-program "gls"))
 
 (use-package dired
-  :preface
-  (defun forge/dired-mode-hook ()
-    "Set misc settings in dired mode."
-    (setq-local truncate-lines t)
-    (forge/turn-on-hl-line))
+    :defer t
+    :preface
+    (defun forge/dired-mode-hook ()
+      "Set misc settings in dired mode."
+      (setq-local truncate-lines t)
+      (forge/turn-on-hl-line))
 
-  (defun forge/dired-up ()
-    "Move up a directory without opening a new buffer."
-    (interactive)
-    (find-alternate-file ".."))
+    (defun forge/dired-up ()
+      "Move up a directory without opening a new buffer."
+      (interactive)
+      (find-alternate-file ".."))
 
-  :bind
-  (("C-c d" . dired-jump)
-    :map dired-mode-map
-    ("RET" . dired-find-alternate-file)
-    ("Y" . forge/dired-rsync)
-    ("^" . forge/dired-up))
+    :bind
+    (("C-c d" . dired-jump)
+     :map dired-mode-map
+     ("RET" . dired-find-alternate-file)
+     ("Y" . forge/dired-rsync)
+     ("^" . forge/dired-up))
 
-  :hook
-  (dired-mode . forge/dired-mode-hook)
+    :hook
+    (dired-mode . forge/dired-mode-hook)
 
-  :diminish dired-omit-mode
+    :diminish dired-omit-mode
 
-  :config
-  (put 'dired-find-alternate-file 'disabled nil)
-  (setq
-    dired-dwim-target t
-    dired-ls-F-marks-symlinks t
-    dired-use-ls-dired t
-    dired-recursive-copies 'always
-    dired-recursive-deletes 'top
-    global-auto-revert-non-file-buffers t ;; auto refresh dired buffers
-    auto-revert-verbose nil))
+    :config
+    (put 'dired-find-alternate-file 'disabled nil)
+    (setq dired-dwim-target t
+          dired-ls-F-marks-symlinks t
+          dired-use-ls-dired t
+          dired-recursive-copies 'always
+          dired-recursive-deletes 'top
+          global-auto-revert-non-file-buffers t ;; auto refresh dired buffers
+          auto-revert-verbose nil))
   
 
 (use-package dired-x 
-  :after dired
-  :config
-  (add-to-list 'dired-omit-extensions ".DS_Store"))
+    :after dired
+    :config
+    (add-to-list 'dired-omit-extensions ".DS_Store"))
 
 (use-package async :ensure t)
 (autoload 'dired-async-mode "dired-async.el" nil t)
@@ -76,22 +76,21 @@
 ;;;
 (defun forge/dired-rsync (dest)
   (interactive
-    (list
-      (expand-file-name
-        (read-file-name "Rsync to:" (dired-dwim-target-directory)))))
+   (list
+    (expand-file-name
+     (read-file-name "Rsync to:" (dired-dwim-target-directory)))))
   ;; store all selected files into "files" list
-  (let ((files (dired-get-marked-files
-                 nil current-prefix-arg))
-         ;; the rsync command
-         (forge/rsync-command "rsync -arvz --progress "))
+  (let ((files (dired-get-marked-files nil current-prefix-arg))
+        ;; the rsync command
+        (forge/rsync-command "rsync -arvz --progress "))
     ;; add all selected file names as arguments
     ;; to the rsync command
     (dolist (file files)
       (setq forge/rsync-command
-        (concat forge/rsync-command (shell-quote-argument file) " ")))
+            (concat forge/rsync-command (shell-quote-argument file) " ")))
     ;; append the destination
     (setq forge/rsync-command
-      (concat forge/rsync-command (shell-quote-argument dest)))
+          (concat forge/rsync-command (shell-quote-argument dest)))
     ;; run the async shell command
     (async-shell-command forge/rsync-command "*rsync*")
     ;; finally, switch to that window
