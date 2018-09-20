56661021a1cf7542b2065b0cd25cb4cfd300c193
Stephen Fromm <stephenf@nero.net>
2018-09-20 14:32:26 -0700

    add modules chat and elfeed

---
+++ modules/forge-chat.el
@@ -0,0 +1,151 @@
+;;; forge-chat.el --- This is an Emacs Lisp file with Emacs Lisp code. -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Stephen Fromm
+
+;; Author: Stephen Fromm
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+(defvar forge-jabber-account-alist nil
+  "Alist of jabber account definitions.")
+
+(use-package jabber
+    :ensure t
+    :preface
+    (defun forge/jabber-start-or-switch ()
+      "Connect to Jabber services"
+      (interactive)
+      (unless (get-buffer "*-jabber-roster-*")
+        (jabber-connect-all))
+      (if (or nil jabber-activity-jids)
+          (jabber-activity-switch-to)
+        (jabber-switch-to-roster-buffer)))
+    :hook
+    (jabber-post-connect . jabber-autoaway-start)
+    :config
+    (setq jabber-account-list forge-jabber-account-alist
+          jabber-auto-reconnect t  ; reconnect automatically
+          jabber-history-dir (concat forge-log-dir "jabber")
+          jabber-history-enabled t ; enable logging
+          jabber-history-muc-enabled t
+          jabber-use-global-history nil
+          jabber-backlog-number 40
+          jabber-backlog-days 30
+          jabber-chat-buffer-show-avatar t ; show avatar in chat buffer
+          jabber-vcard-avatars-retrieve t ; automatically download vcard avatars
+          jabber-alert-info-message-hooks (quote (jabber-info-echo jabber-info-display))
+          jabber-alert-message-hooks (quote (jabber-message-notifications jabber-message-echo jabber-message-scroll))
+          jabber-alert-presence-hooks (quote ()) ; don't show anything on presence changes
+          jabber-alert-muc-hooks (quote (jabber-muc-notifications-personal jabber-muc-echo jabber-muc-scroll)))
+                                        ; jabber uses the fsm package
+    (setq fsm-debug nil)       ; defaults to "*fsm-debug*"
+    (dolist (hook '(jabber-chat-mode-hook jabber-roster-mode-hook))
+      (add-hook hook (lambda () "Disable yasnippet in jabber" (setq yas-dont-activate t)))))
+
+
+(require 'notifications)
+(require 'tls)
+
+(defvar forge-erc-nick user-login-name
+  "Default IRC login name.")
+
+(defvar forge-erc-away-nick (concat forge-erc-nick "|afk")
+  "Default IRC nick when away.")
+
+(defvar forge-erc-keywords nil
+  "Default keywords to look for.")
+
+(defvar forge-erc-channels-alist nil
+  "Default channels to log into.")
+
+(use-package erc
+    :defer t
+    :preface
+    (defun sf/erc-connect ()
+      "Connect to IRC via ERC"
+      (interactive)
+      (when (y-or-n-p "Connect to freenode? ")
+        (erc-tls :server "irc.freenode.net" :port 6697))
+      (when (y-or-n-p "Connect to bitlbee? ")
+        (progn
+          (use-package bitlbee :demand t)
+          (bitlbee-start)
+          (sleep-for 2)
+          (erc :server "localhost" :port 6667))))
+
+    :config
+    (use-package erc-match
+        :after erc
+        :config
+        (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
+                                        "324" "329" "333" "353" "477")))
+
+    (setq erc-modules '(autojoin autoaway button completion fill irccontrols
+                        list log match menu move-to-prompt netsplit
+                        networks notifications readonly ring
+                        services smiley spelling stamp track))
+    (erc-services-mode t)
+    (setq erc-nick forge-erc-nick
+          erc-user-full-name forge-erc-nick
+          erc-away-nickname forge-erc-away-nick
+          erc-keywords forge-erc-keywords
+          erc-auto-join-channels-alist forge-erc-channels-alist
+          erc-insert-timestamp-function 'erc-insert-timestamp-left
+          erc-timestamp-format "%H:%M:%S "
+          ;; kill buffer after channel /part
+          erc-kill-buffer-on-part t
+          ;; kill buffer for server messages after /quit
+          erc-kill-server-buffer-on-quit t
+          ;; autoaway
+          erc-auto-discard-away t
+          erc-autoaway-use-emacs-idle t
+          ;; logging
+          erc-generate-log-file-name-function 'erc-generate-log-file-name-with-date
+          erc-log-channels-directory (concat forge-log-dir "erc")
+          erc-log-insert-log-on-open nil
+          erc-prompt-for-nickserv-password nil
+          erc-save-buffer-on-part t))
+
+
+(defun bitlbee-netrc-identify ()
+  "Auto-identify for Bitlbee channels using authinfo or netrc.
+
+   The entries that we look for in netrc or authinfo files have
+   their 'port' set to 'bitlbee', their 'login' or 'user' set to
+   the current nickname and 'server' set to the current IRC
+   server's name.  A sample value that works for authenticating
+   as user 'keramida' on server 'localhost' is:
+
+   machine localhost port bitlbee login keramida password supersecret"
+  (interactive)
+  (when (string= (buffer-name) "&bitlbee")
+    (let* ((secret (plist-get (nth 0 (auth-source-search :max 1
+                                       :host erc-server
+                                       :user (erc-current-nick)
+                                       :port "bitlbee"))
+                     :secret))
+            (password (if (functionp secret)
+                        (funcall secret)
+                        secret)))
+      (erc-message "PRIVMSG" (concat (erc-default-target) " " "identify" " " password) nil))))
+;; Enable the netrc authentication function for &biblbee channels.
+(add-hook 'erc-join-hook 'bitlbee-netrc-identify)
+
+(provide 'forge-chat)
+;;; forge-chat.el ends here
---
+++ modules/forge-elfeed.el
@@ -0,0 +1,153 @@
+;;; forge-elfeed.el --- Configure elfeed RSS reader.  -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Stephen Fromm
+
+;; Author: Stephen Fromm
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(use-package youtube-dl
+    :init (setq youtube-dl-directory "~/annex/Video/youtube"))
+
+(use-package elfeed
+    :ensure elfeed
+    :bind (:map elfeed-search-mode-map
+                ("d" . elfeed-search-youtube-dl)
+                ("f" . forge/elfeed-search-toggle-starred)
+                ("o" . elfeed-search-mpv)
+                ("F" . forge/elfeed-search-starred)
+                ("<" . forge/elfeed-search-first-article)
+                (">" . forge/elfeed-search-last-article)
+                :map elfeed-show-mode-map
+                ("d" . elfeed-show-youtube-dl)
+                ("f" . forge/elfeed-show-toggle-starred)
+                ("o" . elfeed-show-mpv))
+    :config
+    (defun elfeed-search-mpv ()
+      "Play the current entry with mpv"
+      (interactive)
+      (message "url %s" (elfeed-entry-link (car (elfeed-search-selected))))
+      (start-process "*elfeed-mpv*" nil "mpv" (elfeed-entry-link (car (elfeed-search-selected)))))
+
+    (defun elfeed-show-mpv ()
+      "Play the current entry with mpv"
+      (interactive)
+      (start-process "*elfeed-mpv*" nil "mpv" (elfeed-entry-link elfeed-show-entry)))
+
+    ;; from skeeto
+    ;; https://github.com/skeeto/.emacs.d/blob/master/etc/feed-setup.el
+    (defun elfeed-search-youtube-dl ()
+      "Download the current entry/entries with youtube-dl"
+      (interactive)
+      (let ((entries (elfeed-search-selected)))
+        (dolist (entry entries)
+          (if (null (youtube-dl (elfeed-entry-link entry)
+                                :title (elfeed-entry-title entry)))
+              (message "Entry is not a youtube link")
+            (message "Downloading %s" (elfeed-entry-title entry)))
+          (elfeed-untag entry 'unread)
+          (elfeed-search-update-entry entry)
+          (unless (use-region-p) (forward-line)))))
+
+    ;; from skeeto
+    ;; https://github.com/skeeto/.emacs.d/blob/master/etc/feed-setup.el
+    (defun elfeed-show-youtube-dl ()
+      "Download the current entry with youtube-dl"
+      (interactive)
+      (pop-to-buffer (youtube-dl (elfeed-entry-link elfeed-show-entry))))
+
+    (defun forge/elfeed-search-starred ()
+      "Show starred elfeed articles"
+      (interactive)
+      (elfeed-search-set-filter "@6-months-ago +starred"))
+
+    (defalias 'forge/elfeed-search-toggle-starred (elfeed-expose #'elfeed-search-toggle-all 'starred))
+
+    (defun forge/elfeed-show-toggle-starred ()
+      "Toggle starred tag for elfeed article"
+      (interactive)
+      (forge/elfeed-show-toggle-tag 'starred))
+
+    (defun forge/elfeed-show-toggle-tag (tag)
+      "Toggle tag for elfeed article."
+      (interactive)
+      (if (elfeed-tagged-p tag elfeed-show-entry)
+          (elfeed-show-untag tag)
+        (elfeed-show-tag tag)))
+
+    (defun forge/elfeed-update ()
+      "Update elfeed database."
+      (message "Updating elfeed articles...")
+      (elfeed-update)
+      (elfeed-db-save))
+
+    (defun forge/elfeed-search-first-article ()
+      "Go to first message in search."
+      (interactive)
+      (goto-char (point-min)))
+
+    (defun forge/elfeed-search-last-article ()
+      "Go to last message in search."
+      (interactive)
+      (goto-char (point-max)))
+
+    (defface elfeed-search-starred-title-face '((t :foreground "#f77"))
+      "Marks a starred Elfeed entry.")
+    (push '(starred elfeed-search-starred-title-face) elfeed-search-face-alist)
+
+    (setq url-queue-timeout 30
+          elfeed-db-directory (concat forge-state-dir "elfeed")
+          ;; create timer to update elfeed
+          elfeed-update-timer (run-at-time 180 (* 120 60) 'forge/elfeed-update)
+          ;; configure feeds
+          elfeed-feeds '(("http://codeascraft.com/feed/" devops)
+                         ("https://rachelbythebay.com/w/atom.xml" blog)
+                         ("https://jpmens.net/atom.xml" blog)
+                         ("http://planetsysadmin.com/atom.xml" devops )
+                         ("https://feeds.feedburner.com/sysadvent" devops )
+                         ("https://blog.miguelgrinberg.com/feed" python )
+                         ("http://kernelplanet.org/rss20.xml" linux )
+                         ("http://planet.freedesktop.org/rss20.xml" linux )
+                         ("http://feeds.networklore.com/Networklore" netops )
+                         ("http://etherealmind.com/feed/" netops)
+                         ;; emacs
+                         ("http://endlessparentheses.com/atom.xml" emacs)
+                         ("http://planet.emacsen.org/atom.xml" emacs)
+                         ("http://oremacs.com/atom.xml" emacs)
+                         ("http://pragmaticemacs.com/feed/" emacs)
+                         ("http://mbork.pl/?action=rss" emacs )
+                         ("http://www.howardism.org/index.xml" emacs blog )
+                         ("http://feeds.feedburner.com/TheKitchinResearchGroup" emacs )
+                         ("http://sachachua.com/blog/feed" emacs blog )
+                         ("http://blog.aaronbieber.com/feed.xml" emacs blog )
+                         ("https://www.masteringemacs.org/feed" emacs )
+                         ("http://karl-voit.at/feeds/lazyblorg-all.atom_1.0.links-and-content.xml" emacs)
+                         ("http://irreal.org/blog/?feed=rss2" emacs)
+                         ("https://changelog.complete.org/feed" emacs blog)
+                         ;; amusement
+                         ("http://xkcd.com/atom.xml" comics fun)
+                         ("https://what-if.xkcd.com/feed.atom" fun)
+                         ;; misc
+                         ("http://americasgreatoutdoors.tumblr.com/rss" blog)
+                         ("https://www.youtube.com/feeds/videos.xml?user=polygon" youtube)
+                         ("http://git-annex.branchable.com/tips/index.atom" gitannex)
+                         ("http://git-annex.branchable.com/devblog/index.atom" gitannex))))
+
+
+(provide 'forge-elfeed)
+;;; forge-elfeed.el ends here
