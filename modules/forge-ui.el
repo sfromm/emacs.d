aac27dc026c499a2ff43c8cc06b40ddebaa0ef16
Stephen Fromm <stephenf@nero.net>
2019-09-20 21:14:32 -0700

    add dumb-jump bindings

--- modules/forge-ui.el
+++ modules/forge-ui.el
@@ -91,10 +91,13 @@
 
 (use-package dumb-jump
     :defer t
+    :ensure t
     :bind
     (("M-g o" . dumb-jump-go-other-window)
      ("M-g j" . dumb-jump-go)
      ("M-g i" . dumb-jump-go-prompt)
+     ("M-g b" . dumb-jump-back)
+     ("M-g q" . dumb-jump-quick-look)
      ("M-g x" . dumb-jump-go-prefer-external)
      ("M-g z" . dumb-jump-go-prefer-external-other-window))
     :config
