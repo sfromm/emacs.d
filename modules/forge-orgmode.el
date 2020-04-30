8c936d533e68cf30647acaeca615b80565e1f635
Stephen Fromm <stephenf@nero.net>
2020-04-30 13:53:42 -0700

    catch invisible edits when headings are folded

--- modules/forge-orgmode.el
+++ modules/forge-orgmode.el
@@ -105,6 +105,7 @@
   :custom
   (org-refile-use-outline-path 'file)
   (org-outline-path-complete-in-steps nil)
+  (org-catch-invisible-edits 'smart)
   (org-refile-targets (quote ((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))))
 
   :bind (("<f8>" . org-cycle-agenda-files)
