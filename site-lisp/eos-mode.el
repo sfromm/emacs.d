30be59d67426c4b6628397ed94efae10fd25ae79
Stephen Fromm <stephenf@nero.net>
2020-10-23 16:31:16 -0700

    updates to eos mode

--- site-lisp/eos-mode.el
+++ site-lisp/eos-mode.el
@@ -3,26 +3,29 @@
 ;; Copyright (C) 2020 Stephen Fromm <sfromm at gmail.com>
 ;; Copyright (C) 2004 Noufal Ibrahim <nkv at nibrahim.net.in>
 ;;
-;; This program is not part of Gnu Emacs
+;; This program is not part of GNU Emacs
 ;;
-;; eos-mode.el is free software; you can
-;; redistribute it and/or modify it under the terms of the GNU General
-;; Public License as published by the Free Software Foundation; either
-;; version 2 of the License, or (at your option) any later version.
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
 ;;
-;; This program is distributed in the hope that it will be useful,
+;; This file is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-;;; Notes:
+;; You should have received a copy of the GNU General Public License along
+;; with this program; if not, write to the Free Software Foundation, Inc.,
+;; 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+;;
+;;; Commentary:
 ;; Based on ios-config-mode.el from https://github.com/nibrahim/IOS-config-mode
+;; by Noufal Ibrahim.
 
 ;;; Code:
+
+(require 'rx)
 
 (defgroup eos-mode nil
   "EOS Mode"
@@ -39,124 +42,115 @@
     map)
   "Keymap for Arista router configuration major mode.")
 
-;; Font locking definitions.
-(defvar eos-command-face 'eos-command-face "Face for basic router commands.")
+
+;; Constants
 
-(defface eos-command-face
-  '(
-    (((type tty) (class color)) (:foreground "cyan"))
-    (((type graphic) (class color)) (:foreground "cyan"))
-    (t (:foreground "cyan" ))
-    )
-  "Face for basic router commands")
+(defconst eos-section-regex
+  (concat
+   "^ *"
+   (regexp-opt
+    '("controller" "hardware tcam" "interface" "ip routing vrf"
+      "ip access-list" "ip prefix-list" "ipv6 prefix-list"
+      "line" "management api" "policy-map" "redundancy" "route-map" "router") t)))
 
-(defvar eos-toplevel-face 'eos-toplevel-face "Face for top level commands.")
+(defconst eos-keywords-regex
+  (regexp-opt
+   '("aaa" "description" "isis" "logging" "neighbor" "vlan") 'words)
+  "Regular expressions for EOS keywords.")
 
-(defface eos-toplevel-face
-  '(
-    (((type tty) (class color)) (:foreground "blue"))
-    (((type graphic) (class color)) (:foreground "lightsteelblue"))
-    (t (:foreground "lightsteelblue" ))
-    )
-  "Face for basic router commands")
+(defconst eos-commands-regex
+  (concat
+   "^ *"
+   (regexp-opt
+    '("ip community-list" "ip radius" "ip routing" "ip route"
+      "ntp server" "radius-server" "snmp-server") t))
+  "Regular expressions for EOS commands.")
 
-(defvar eos-shutdown-face 'eos-shutdown-face "Face for \"shutdown\".")
+(defconst eos-ipaddr-regex
+  (rx
+   (or
+    (group (repeat 1 3 digit) "\."
+           (repeat 1 3 digit) "\."
+           (repeat 1 3 digit) "\."
+           (repeat 1 3 digit)
+           (optional "/" (one-or-more digit)))
+    (group "::/" (one-or-more digit))))
+  "RegExp for an IP address.")
 
-(defface eos-shutdown-face
-  '(
-    (((type tty) (class color)) (:foreground "red"))
-    (((type graphic) (class color)) (:foreground "red"))
-    (t (:foreground "red" ))
-    )
-  "Face for shutdown commands")
+(defconst eos-no-regex
+  (regexp-opt '("no") 'words)
+  "Regular expression for `no' commands.")
 
-(defvar eos-no-face 'eos-no-face "Face for \"no\".")
+(defconst eos-shutdown-regex
+  (regexp-opt '("shutdown") 'words)
+  "Regular expression for `shutdown' command.")
 
-(defface eos-no-face
-  '(
-    (t (:underline t))
-    )
-  "Face for \"no\"")
-
-(defvar eos-ipadd-face 'eos-ipadd-face "Face for IP addresses.")
-
-(defface eos-ipadd-face
-  '(
-    (((type tty) (class color)) (:foreground "yellow"))
-    (((type graphic) (class color)) (:foreground "LightGoldenrod"))
-    (t (:foreground "LightGoldenrod" ))
-    )
-  "Face for IP addresses")
-
-;; (regexp-opt '("interface" "ip vrf" "controller" "class-map" "redundancy" "line" "policy-map" "router" "access-list" "route-map") t)
-;; (regexp-opt '("diagnostic" "hostname" "logging" "service" "alias" "snmp-server" "boot" "card" "vtp" "version" "enable") t)
-
-(defconst eos-font-lock-keywords
+(defvar eos-font-lock-keywords
   (list
-   '( "\\<\\(access-list\\|c\\(?:lass-map\\|ontroller\\)\\|i\\(?:nterface\\|p vrf\\)\\|line\\|policy-map\\|r\\(?:edundancy\\|oute\\(?:-map\\|r\\)\\)\\)\\>". eos-toplevel-face)
-   '( "\\<\\(alias\\|boot\\|card\\|diagnostic\\|^enable\\|hostname\\|logging\\|s\\(?:ervice\\|nmp-server\\)\\|v\\(?:ersion\\|tp\\)\\)\\>" . eos-command-face)
-   '("\\<\\(no\\)\\>" . eos-no-face)
-   '("^ *\\<\\(shutdown\\)\\>" . eos-shutdown-face)
-   '("\\<\\([0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\(?:/[0-9]\\+\\)\\)\\>" . eos-ipadd-face)
+   (list eos-section-regex 0 font-lock-function-name-face)
+   (list eos-keywords-regex 0 font-lock-keyword-face)
+   (list eos-commands-regex 0 font-lock-builtin-face)
+   (list eos-ipaddr-regex 0 font-lock-string-face)
+   (list eos-no-regex 0 font-lock-warning-face)
+   (list eos-shutdown-regex 0 font-lock-warning-face)
    )
-  "Font locking definitions for Arista eos mode")
+  "Font locking definitions for Arista eos mode.")
+
+(defun eos-indent-line ()
+  "Indent current line as arista eos config line."
+  (let ((indent0 "^interface\\|redundancy\\|^line\\|^ip vrf \\|^controller\\|^class-map\\|^policy-map\\|router\\|access-list\\|route-map")
+        (indent1 " *main-cpu\\| *class\\W"))
+    (beginning-of-line)
+    (let ((not-indented t)
+          (cur-indent 0))
+      (cond ((or (bobp) (looking-at indent0) (looking-at "!")) ; Handles the indent0 and indent1 lines
+             (setq not-indented nil
+                   cur-indent 0))
+            ((looking-at indent1)
+             (setq not-indented nil
+                   cur-indent 1)))
+      (save-excursion ; Indents regular lines depending on the block they're in.
+        (while not-indented
+          (forward-line -1)
+          (cond ((looking-at indent1)
+                 (setq cur-indent 2
+                       not-indented nil))
+                ((looking-at indent0)
+                 (setq cur-indent 1
+                       not-indented nil))
+                ((looking-at "!")
+                 (setq cur-indent 0
+                       not-indented nil))
+                ((bobp)
+                 (setq cur-indent 0
+                       not-indented nil)))))
+      (indent-line-to cur-indent))))
+
+
+;; Mode setup
 
 ;; Imenu definitions.
 (defvar eos-imenu-expression
   '(
     ("Interfaces"        "^[\t ]*interface *\\(.*\\)" 1)
-    ("VRFs"              "^ip vrf *\\(.*\\)" 1)
+    ("VRFs"              "^ *vrf *\\(.*\\)" 1)
     ("Controllers"       "^[\t ]*controller *\\(.*\\)" 1)
     ("Routing protocols" "^router *\\(.*\\)" 1)
-    ("Class maps"        "^class-map *\\(.*\\)" 1)
-    ("Policy maps"       "^policy-map *\\(.*\\)" 1)
-    ))
+    )
+  "Imenu configuration for `eos-mode'.")
 
-;; Indentation definitions.
-(defun eos-indent-line ()
-  "Indent current line as arista eos config line"
-  (let ((indent0 "^interface\\|redundancy\\|^line\\|^ip vrf \\|^controller\\|^class-map\\|^policy-map\\|router\\|access-list\\|route-map")
-	(indent1 " *main-cpu\\| *class\\W"))
-    (beginning-of-line)
-    (let ((not-indented t)
-	  (cur-indent 0))
-      (cond ((or (bobp) (looking-at indent0) (looking-at "!")) ; Handles the indent0 and indent1 lines
-	     (setq not-indented nil
-		   cur-indent 0))
-	    ((looking-at indent1)
-	     (setq not-indented nil
-		   cur-indent 1)))
-      (save-excursion ; Indents regular lines depending on the block they're in.
-	(while not-indented
-	  (forward-line -1)
-	  (cond ((looking-at indent1)
-		 (setq cur-indent 2
-		       not-indented nil))
-		((looking-at indent0)
-		 (setq cur-indent 1
-		       not-indented nil))
-		((looking-at "!")
-		 (setq cur-indent 0
-		       not-indented nil))
-		((bobp)
-		 (setq cur-indent 0
-		       not-indented nil)))))
-      (indent-line-to cur-indent))))
-
-
-;; Custom syntax table
-(defvar eos-mode-syntax-table (make-syntax-table)
-  "Syntax table for Arista eos mode.")
-
-(modify-syntax-entry ?_ "w" eos-mode-syntax-table) ;All _'s are part of words.
-(modify-syntax-entry ?- "w" eos-mode-syntax-table) ;All -'s are part of words.
-(modify-syntax-entry ?! "<" eos-mode-syntax-table) ;All !'s start comments.
-(modify-syntax-entry ?\n ">" eos-mode-syntax-table) ;All newlines end comments.
-(modify-syntax-entry ?\r ">" eos-mode-syntax-table) ;All linefeeds end comments.
-
+(defvar eos-mode-syntax-table
+  (let ((syntax-table (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" eos-mode-syntax-table)  ;; All _'s are part of words.
+    (modify-syntax-entry ?- "w" eos-mode-syntax-table)  ;; All -'s are part of words.
+    (modify-syntax-entry ?! "<" eos-mode-syntax-table)  ;; All !'s start comments.
+    (modify-syntax-entry ?\n ">" eos-mode-syntax-table) ;; All newlines end comments.
+    (modify-syntax-entry ?\r ">" eos-mode-syntax-table) ;; All linefeeds end comments.
+    syntax-table)
+  "Syntax table for Arista `eos-mode'.")
 
 ;;;###autoload
-(define-derived-mode eos-mode c-mode "EOS"
+(define-derived-mode eos-mode text-mode "EOS"
   "Major mode for Arista EOS (TM) configuration files"
   :syntax-table eos-mode-syntax-table
   :group 'eos-mode
