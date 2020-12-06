0f8629d1b46912736d44600a44af34108059f1e6
Stephen Fromm <stephenf@nero.net>
2020-12-06 09:21:49 -0800

    ensure diff-hl

--- modules/forge-editing.el
+++ modules/forge-editing.el
@@ -108,6 +108,7 @@
 ;;; diff-hl
 ;;; https://github.com/dgutov/diff-hl
 (use-package diff-hl
+  :ensure t
   :commands (diff-hl-mode diff-hl-dired-mode)
   :hook (magit-post-refresh . diff-hl-magit-post-refresh))
 
