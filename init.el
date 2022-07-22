debca17973078bcbdc5036a68d58f58b6803de05
Stephen Fromm <stephenf@nero.net>
2022-07-22 16:37:43 -0700

    sudo this file - reopen file as root

--- init.el
+++ init.el
@@ -381,6 +381,34 @@ end tell")
   (interactive)
   (untabify (point-min) (point-max)))
 
+(defun forge/sudo-file-path (file)
+  "Return path for FILE with sudo access."
+  (let ((host (or (file-remote-p file 'host) "localhost")))
+    (concat "/" (when (file-remote-p file)
+                  (concat (file-remote-p file 'method) ":"
+                          (if-let (user (file-remote-p file 'user))
+                              (concat user "@" host) host)
+                          "|"))
+            "sudo:root@" host
+            ":" (or (file-remote-p file 'localname)
+                    file))))
+
+(defun forge/sudo-find-file (file)
+  "Open FILE as root."
+  (interactive "FOpen file as root: ")
+  (find-file (forge/sudo-file-path file)))
+
+(defun forge/sudo-this-file ()
+  "Open current file as root."
+  (interactive)
+  (find-file
+   (forge/sudo-file-path
+    (or buffer-file-name
+        (or buffer-file-name
+            (when (or (derived-mode-p 'dired-mode)
+                      (derived-mode-p 'wdired-mode))
+              default-directory))))))
+
 (use-package net-utils
   :straight (:type built-in)
   :custom
