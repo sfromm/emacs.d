7f8b61c6b9e79397a259df6f5737d9f164b690e2
Stephen Fromm <stephenf@nero.net>
2022-08-08 16:21:44 -0700

    continue to fiddle with tab-bar settings

--- init.el
+++ init.el
@@ -1018,12 +1018,13 @@ Query for DNS records for DOMAIN of QUERY-TYPE."
 (when (require 'tab-bar nil 'noerror)
   (tab-bar-mode)
   (setq tab-bar-close-tab-select 'recent
+        tab-bar-close-button-show nil
         tab-bar-new-tab-choice 'ibuffer
         tab-bar-new-tab-to 'right
         tab-bar-position nil
         tab-bar-select-tab-modifiers '(super meta)
         tab-bar-tab-hints t
-        tab-bar-show t))
+        tab-bar-show 1))
 
 ;; https://gitlab.com/protesilaos/dotfiles/-/blob/master/emacs/.emacs.d/prot-lisp/prot-tab.el
 (defun prot-tab--tab-bar-tabs ()
