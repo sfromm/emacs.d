f25560de98862001698d5b260fe64d4cc5671cbf
Stephen Fromm <stephenf@nero.net>
2025-08-08 16:58:34 -0700

    minor tweaks to build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ BUILD_PATH="$HOME/src/emacs"
 EMACS_URL="https://ftpmirror.gnu.org/emacs/emacs-${VERSION}.tar.gz"
 CPU_COUNT=$(nproc --all)
 
+7_days_ago=$(date -d 'now - 7 days' +%s)
+apt_last_update=$(date -r "/var/cache/apt/pkgcache.bin" +%s)
+
 set -e  # exit immediately if pipeline exit non-zero
 set -x  # display command to stdout
 
@@ -15,7 +18,9 @@ cd $BUILD_PATH
 [[ -d emacs-${VERSION} ]]        || tar xzf emacs-${VERSION}.tar.gz
 cd emacs-${VERSION}
 
-sudo apt update
+if (( apt_last_update <= 7_days_ago )); then
+    sudo apt update
+fi
 sudo apt-get install -y libmagickwand-dev
 sudo apt-get install -y libgccjit0 libgccjit-12-dev libjansson4 libjansson-dev libtree-sitter-dev
 sudo apt-get build-dep emacs -y
